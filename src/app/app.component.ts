83b37bbc79a6f0e4afc745adb7d1ed3127bfdb05
Robin Schellius <r.schellius@avans.nl>
2022-11-07 16:06:32 +0100

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-demo';
+    title = "angular-demo";
+    pi = 3.14;
+
+    getTitle() {
+        return this.title;
+    }
 }
